467c1696eae72b3feb123674026164f752ea0671
Arman B <arman.baratifar@utexas.edu>
2017-07-14 17:58:42 -0500

    More initial setup

--- graphius/cli.py
+++ graphius/cli.py
@@ -1,17 +1,12 @@
 # -*- coding: utf-8 -*-
 
 """Console script for graphius."""
+import graphius
 
-import click
 
-
-@click.command()
 def main(args=None):
-    """Console script for graphius."""
-    click.echo("Replace this message by putting your code into "
-               "graphius.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
-
+    print("Welcome to Graphius.")
+    g = graphius.Graphius("Graph 1")
 
 if __name__ == "__main__":
     main()
--- graphius/graphius.py
+++ graphius/graphius.py
@@ -1,3 +1,10 @@
 # -*- coding: utf-8 -*-
+import json
 
-"""Main module."""
+
+class Graphius(object):
+    """
+        A class representing a graph.
+    """
+    def __init__(self, name=None):
+        self.name = name
